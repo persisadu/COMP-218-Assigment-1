c5e729af509785c15e1a9d5849a8b226b7879ef9
persisadu <persis.adutwum@gmail.com>
2025-08-06 20:22:02 +0000

    Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -1,12 +1,17 @@
 # Program to find the biggest of three numbers
 
-#Input: Read three numbers from the user
+# Ask uuser to input three numbers
 num1 = float(input("Enter the first number:"))
 num2 = float(input("Enter the second number:"))
 num3 = float(input("Enter the third number:"))
 
 # Find the largest number
-largest = max(num1, num2, num3)
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
 
 # Output the result
-print("The biggest number is:", largest)
+print("The largest number is:", largest)
